0de9f5bbeb824d33048a686524e78ef075b55135
Christmas <slinko.art@mail.ru>
2024-08-15 21:52:53 +0300

    added app entity

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"context"
-	"user-rest-api/internal/config"
-	"user-rest-api/internal/handlers"
-	"user-rest-api/internal/repository/postgres"
-	"user-rest-api/internal/server"
-	"user-rest-api/internal/service"
-	"user-rest-api/pkg/dbclient"
-	"user-rest-api/pkg/logger"
-
-	"github.com/julienschmidt/httprouter"
+	"user-rest-api/internal/app"
 )
 
 func main() {
-	logger.InitLogger()
-	logger := logger.GetLogger()
-
-	logger.Info("config initializing")
-	cfg := config.GetConfig()
-
-	postgreSQLClient := dbclient.NewClient(context.TODO(), cfg.Storage)
-	repository := postgres.NewUsersRepo(postgreSQLClient, logger)
-
-	usersService := service.NewUsersService(logger, repository)
-
-	logger.Info("router initializing")
-	router := httprouter.New()
-
-	logger.Info("handler initializing")
-	handler := handlers.NewUserHandler(logger, usersService)
-	handler.Register(router)
-
-	logger.Info("application running")
-	server.Run(router, cfg)
+	app.Run()
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,10 +2,15 @@ package app
 
 import (
 	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
 	"user-rest-api/internal/config"
 	"user-rest-api/internal/handlers"
 	"user-rest-api/internal/repository"
 	"user-rest-api/internal/repository/postgres"
+	"user-rest-api/internal/service"
 	"user-rest-api/pkg/dbclient"
 	"user-rest-api/pkg/logger"
 
@@ -13,15 +18,17 @@ import (
 )
 
 type App struct {
-	cfg     *config.Config
-	handler handlers.Handler
-	//service service.Service
+	cfg        *config.Config
+	handler    http.Handler
+	service    *service.UsersService
 	repository repository.Users
 }
 
-func NewApp() (App, error) {
+// NewApp initializes the whole application
+func NewApp() App {
 	logger.InitLogger()
 	logger := logger.GetLogger()
+	logger.Info("initializing application")
 
 	logger.Info("config initializing")
 	cfg := config.GetConfig()
@@ -29,22 +36,58 @@ func NewApp() (App, error) {
 	postgreSQLClient := dbclient.NewClient(context.TODO(), cfg.Storage)
 	repository := postgres.NewUsersRepo(postgreSQLClient, logger)
 
+	usersService := service.NewUsersService(logger, repository)
+
 	logger.Info("router initializing")
 	router := httprouter.New()
 
 	logger.Info("handler initializing")
-	handler := handlers.NewUserHandler(logger)
+	handler := handlers.NewUserHandler(logger, usersService)
 	handler.Register(router)
 
 	return App{
 		cfg:        cfg,
-		handler:    handler,
+		handler:    router,
+		service:    usersService,
 		repository: repository,
-	}, nil
+	}
 }
 
-// Run initializes whole application.
-func (a *App) Run(ctx context.Context) error {
-	// TODO
+// startHTTP initializes and start http server
+// TODO: Handle HTTP server shutdown gracefully
+// by goroutine, channel and signals
+func (a *App) startHTTP() error {
+	logger := logger.GetLogger()
+	logger.Info("HTTP server initializing")
+
+	logger.Infof("bind application to %s:%s", a.cfg.Connection.BindIP, a.cfg.Connection.Port)
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%s", a.cfg.Connection.BindIP, a.cfg.Connection.Port))
+	if err != nil {
+		return err
+	}
+
+	httpServer := &http.Server{
+		Handler:      a.handler,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	logger.Info("application initialized and started")
+	if err := httpServer.Serve(listener); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+// Run starts the application.
+// TODO: Handle HTTP server shutdown gracefully
+// by goroutine, channel and signals
+func Run() {
+	a := NewApp()
+	logger := logger.GetLogger()
+
+	if err := a.startHTTP(); err != nil {
+		logger.Fatalf("unable to start application due to error: %w", err)
+	}
 }
